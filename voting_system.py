659211f795d4f86eef77066d233bce020c01349a
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2024-12-17 16:39:09 +0000

    voting system

---
+++ voting_system.py
@@ -0,0 +1,12 @@
+#asking for user details
+
+name = input("Enter your name: ")
+Location = input("Enter your Location: ")
+age = int(input("Enter Age: "))
+
+#control flow
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+    
