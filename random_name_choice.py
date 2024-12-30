5e53092bd6c90732ae6d4d75a25caacd789273e5
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2024-12-30 23:54:39 +0000

    random name generator

---
+++ random_name_choice.py
@@ -0,0 +1,12 @@
+# importing a random
+import random
+
+# List of names
+names = ["Samuel", "Owusu", "Gyamfi", "Angelina", "Maatron", "Kwaku"]
+
+# Choose a random name
+name_choice = random.choice(names)
+
+# Print the chosen name
+print(name_choice)
+
