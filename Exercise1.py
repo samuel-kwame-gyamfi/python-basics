02060c02e1d61f8e76d8241722fd62ca646c2fc6
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2025-01-07 10:16:34 +0000

    The basics of Functions

---
+++ Exercise1.py
@@ -0,0 +1,13 @@
+#Write a function to greet the user by name.
+
+#Instructions:
+
+#Define a function called that takes one parameter, (name).
+#Inside the function, create a greeting message that includes the name passed as an argument.
+#Print the greeting message using the print function.
+
+
+def greet_user(name):
+    print("Hello",name)
+
+greet_user(name= "Samuel")
