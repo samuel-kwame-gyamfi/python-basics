85124ba3f7e46f8baad440fb2e73ae9ef1a2d321
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2025-01-07 10:21:19 +0000

    The basics of python Functions

---
+++ Exercise2.py
@@ -0,0 +1,10 @@
+# Define a function that takes two parameters: length and width
+# Inside the function, multiply the length and width to calculate the area
+# Use the return statement to return the calculated area
+
+def calculate_area(length,width):
+    area = length * width
+    return area
+area = calculate_area(5,6)
+
+print(f"The area of the rectangle is {area}")
