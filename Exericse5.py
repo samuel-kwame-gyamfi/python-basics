35d54ab43bfe6c996a222bdf5776857511b0e691
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2025-01-08 23:24:34 +0000

    Pytho Data Structure Basics

---
+++ Exericse5.py
@@ -0,0 +1,8 @@
+# Define a dictionary to store information about a favorite book.
+# Include keys for title, author, and genre.
+# Use a method to retrieve the value of the genre.
+
+favorite_Book = {"Title":"Think And Grow Rich","Author":"Samuel Owusu","Genre":"Self development book"}
+
+retrive = favorite_Book["Genre"]
+print("retrive")
