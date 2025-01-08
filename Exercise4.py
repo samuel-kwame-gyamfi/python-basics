875f9af2360ba90e8890eb733871a0592586f6ad
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2025-01-08 23:15:16 +0000

    Python Data structures basics

---
+++ Exercise4.py
@@ -0,0 +1,8 @@
+# Create a list to store names of favorite fruits.
+# Access the second element in the list.
+# Print the second element.
+
+
+favorite_fruit = ["apple","Mango","Orange", "Tangerine","Banana","Coconut"]
+fruit = favorite_fruit[1]
+print(fruit)
