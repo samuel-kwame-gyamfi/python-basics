b970efac01dae948f54543811b44c9603071d8d5
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2025-01-16 09:42:30 +0000

    birthday function

---
+++ birthday_fuctions.py
@@ -0,0 +1,8 @@
+  """"chatgpt working examples"""
+def birthday_wish():
+    name = input("What is your name?: ")
+    date_of_birth = int(input(" Enter date of birth:"))
+    age = 2025-date_of_birth
+    print(f"Happy {age}th birthday {name} have a fantastic day")
+
+birthday_wish()
